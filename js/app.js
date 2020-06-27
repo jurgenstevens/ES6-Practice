42757d673a11e50a9260765c45ca3e3d7bb0103f
Jurgen <jurgen.stevens@yahoo.com>
2020-06-26 22:33:25 -0500

    Completed pre-ES6 variables example.

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// old javascript
+
+var names = ["Ed", "John", "Mike"];
+var counter = 10;
+var counter = 5;
+
+function sayName() {
+  var name = "Jurgen";
+  console.log(name);
+}
+
+// var sucks because you can declare a variable twice that way which can interfere with code
+
+sayName();
+
+// ES6
+
+const todoList = ["", ""];
