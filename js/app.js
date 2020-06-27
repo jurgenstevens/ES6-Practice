3be0bc494509fe3dff9f70dd4011728ba674ad5a
Jurgen <jurgen.stevens@yahoo.com>
2020-06-26 23:54:57 -0500

    Reviewed concatination.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
 // old javascript
 
-var names = ["Ed", "John", "Mike"];
-var counter = 10;
-var counter = 5;
+// var names = ["Ed", "John", "Mike"];
+// var counter = 10;
+// var counter = 5;
 
-function sayName() {
-  var name = "Jurgen";
-  console.log(name);
-}
+// function sayName() {
+//   var name = "Jurgen";
+//   console.log(name);
+// }
 
 // var sucks because you can declare a variable twice that way which can interfere with code
 
-sayName();
+// sayName();
 
 // ES6
 
-const todoList = ["", ""];
+const todoList = ["Milk", "Cow"];
+// you can't reassign variables with const
+
+// CONCATINATION old JS
+// var name = "Jurgen";
+// console.log("Hello my name is " + name);
+
+// Concatination ES6
+
+const name = "Jurgen";
+console.log(`Hello my name is ${name}`);
