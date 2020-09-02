f2563a9fb2c0b5cb00316acfd09096af676dca3d
Jurgen <jurgen.stevens@yahoo.com>
2020-09-02 18:02:20 -0500

    Did basic math with JavaScript.

--- js/freecodecamp.js
+++ js/freecodecamp.js
@@ -19,8 +19,44 @@ const pi = 3.14
 
 var a; // this is how you declare a variable
 var b = 2; // this is how you assign a variable
-
+console.log(a)
 a = 7; // now a has been assigned
 b = a; // now a's contents have been assigned to b
-
+console.log(a)
 console.log(b)
+
+//  Initializing Variables w/ Assignment Operator
+var a = 9;
+
+// Uninitialized Variables
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a ";
+//Do not change code below this line
+a = a + 1; // => 6
+b = b + 5; // => 15
+c = c + "String!" // "I am a String!"
+
+// Case sensitivity in variables
+// Declarations must always be camelCase
+var studlyCapVar; // correct
+var properCamelCase; // correct
+var titleCaseOver; // correct
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Adding Numbers
+var sum = 10 + 10;
+console.log(sum)
+// Subtracting Numbers
+var difference = 45 - 5;
+console.log(difference)
+// Multiplying Numbers
+var product = 9 * 9;
+console.log(product)
+// Dividing Numbers
+var quotient = 66 / 6;
+console.log(quotient)
