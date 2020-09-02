358af05f5c0689dba3a1e1b0061b6349b387ae2c
Jurgen <jurgen.stevens@yahoo.com>
2020-09-02 17:24:50 -0500

    Discussed, declared and assigned variables.

--- js/freecodecamp.js
+++ js/freecodecamp.js
@@ -1,1 +1,26 @@
 console.log("Hello from Javascript")
+
+// This is a comment
+var number = 5; // this is an in-line comment
+/* this is a
+multi-line comment */
+number = 9;
+
+// Data Types: undefined, null, boolean, string, symbol, number and object
+
+// var can be used throughout the whole program
+var myName = "Jurgen"
+
+// let will only be used within the scope of where it's declared
+let city = "Chicago"
+
+// const is a variable that shouldn't change
+const pi = 3.14
+
+var a; // this is how you declare a variable
+var b = 2; // this is how you assign a variable
+
+a = 7; // now a has been assigned
+b = a; // now a's contents have been assigned to b
+
+console.log(b)
